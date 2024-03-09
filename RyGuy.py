0a4d1d0b598a6d5c604b6ffc5b8bfce1c20fbf9f
Ryan Francis <2yan@outlook.com>
2024-03-09 02:56:43 -0800

    more robust

--- RyGuy.py
+++ RyGuy.py
@@ -20,11 +20,11 @@ import queue
 
 class IRacingStatus(Enum):
     CONNECTED_ON_TRACK = "Connected - On Track!"
-    CONNECTED_NOT_ON_TRACK = "Connected - Not on track"
-    GAME_NOT_STARTED = "Game Not Started"
+    CONNECTED_NOT_ON_TRACK = "Connected - Car Not on track"
+    GAME_NOT_STARTED = "Waiting For Game"
     BOOTING = 'Booting'
     CRASHED = 'CRASHED'
-    GAME_STARTED = "Game Started"
+    GAME_STARTED = "Game Loading"
 
 class SerialStatus(Enum):
     CONNECTED = 'Connected'
@@ -49,52 +49,19 @@ def find_port():
     return False
 
 
-
-
-
 def launch(f):
     t = threading.Thread(target=f)
     t.start()
     return t
 
-            
-def get_text(ir):
-
-    session_time = ir['SessionTime']
-    enter_exit_reset = ir['EnterExitReset']
-    lap = ir['Lap']
-    lap_dist_pct = ir['LapDistPct']
-    on_pit_road = int(ir['OnPitRoad'])
-    speed = ir['Speed']
-    throttle_raw = ir['ThrottleRaw']
-    brake_raw = ir['BrakeRaw']
-    steering_wheel_angle = ir['SteeringWheelAngle']
-    rpm = ir['RPM']
-    velocity_x = ir['VelocityX']
-    velocity_y = ir['VelocityY']
-    velocity_z = ir['VelocityZ']
-    lat_accel = ir['LatAccel']
-    long_accel = ir['LongAccel']
-    vert_accel = ir['VertAccel']
-    yaw_rate = ir['YawRate']
-    pitch_rate = ir['PitchRate']
-    roll_rate = ir['RollRate']
-    pitch = ir['Pitch']
-    roll = ir['Roll']
-    yaw_north = ir['YawNorth']
-    
-    text = f"$IRTEL,{session_time:.3f},{enter_exit_reset:d},{lap:d},{lap_dist_pct:f},{on_pit_road:d},0.000000,0.000000,0.000000,{speed:f},{throttle_raw:f},{brake_raw:f},{steering_wheel_angle:f},{rpm:f},{velocity_x:f},{velocity_y:f},{velocity_z:f},{lat_accel:f},{long_accel:f},{vert_accel:f},{yaw_rate:f},{pitch_rate:f},{roll_rate:f},{pitch:f},{roll:f},{yaw_north:f}"
-
-    return text 
+     
 
 
     
     
-class Robot:
-    
-        
+class Robot:        
     def __init__(self):
-
+        
         # Initalize variables
         self.serial_status = SerialStatus.BOOTING
         self.iracing_status = IRacingStatus.CONNECTED_ON_TRACK
@@ -110,13 +77,44 @@ class Robot:
         self.stop_pull = False
         self.stop_send = False
         
+        self.max_time = 0
         
-        
+        print("Booted")
+
         return 
     
-
-
-
+    
+    
+               
+    def get_text(self):
+        ir = self.ir
+        session_time = ir['SessionTime']
+        enter_exit_reset = ir['EnterExitReset']
+        lap = ir['Lap']
+        lap_dist_pct = ir['LapDistPct']
+        on_pit_road = int(ir['OnPitRoad'])
+        speed = ir['Speed']
+        throttle_raw = ir['ThrottleRaw']
+        brake_raw = ir['BrakeRaw']
+        steering_wheel_angle = ir['SteeringWheelAngle']
+        rpm = ir['RPM']
+        velocity_x = ir['VelocityX']
+        velocity_y = ir['VelocityY']
+        velocity_z = ir['VelocityZ']
+        lat_accel = ir['LatAccel']
+        long_accel = ir['LongAccel']
+        vert_accel = ir['VertAccel']
+        yaw_rate = ir['YawRate']
+        pitch_rate = ir['PitchRate']
+        roll_rate = ir['RollRate']
+        pitch = ir['Pitch']
+        roll = ir['Roll']
+        yaw_north = ir['YawNorth']
+        
+        text = f"$IRTEL,{session_time:.3f},{enter_exit_reset:d},{lap:d},{lap_dist_pct:f},{on_pit_road:d},0.000000,0.000000,0.000000,{speed:f},{throttle_raw:f},{brake_raw:f},{steering_wheel_angle:f},{rpm:f},{velocity_x:f},{velocity_y:f},{velocity_z:f},{lat_accel:f},{long_accel:f},{vert_accel:f},{yaw_rate:f},{pitch_rate:f},{roll_rate:f},{pitch:f},{roll:f},{yaw_north:f}"
+    
+        return text 
+    
     def run(self):
         i = 0
         while True:
@@ -155,7 +153,7 @@ class Robot:
                     self.set_port()
                     
                 if self.port and (self.serial_status == SerialStatus.DEVICE_FOUND):
-                    self.serial = serial.Serial(self.port, 115200,)
+                    self.serial = serial.Serial(self.port, 115200, timeout=3)
                     self.serial_status = SerialStatus.CONNECTED
                 
             elif self.plugged_in():
@@ -170,6 +168,7 @@ class Robot:
             if not(self.data_pull_thread):
                 if (self.iracing_status == IRacingStatus.CONNECTED_ON_TRACK) and  (self.serial_status == SerialStatus.CONNECTED):
                     self.data_pull_thread = launch(self.data_pull_loop)
+
             else:
                 if not(self.data_pull_thread.is_alive()):
                     self.IRacingStatus = IRacingStatus.CRASHED
@@ -183,6 +182,20 @@ class Robot:
                     self.serial_status = SerialStatus.DISCONNECTED
                     self.data_send_thread = False
                 
+            if (self.iracing_status != IRacingStatus.CONNECTED_ON_TRACK) and (self.serial_status == SerialStatus.CONNECTED):
+                blank = iracing.get_blank()
+                self.max_time = self.max_time + .1
+                blank = blank.replace('TIME', f'{self.max_time:.3f}')
+                if '\n' not in blank:
+                    blank = blank + '\n'
+                time.sleep(.1)
+                try:
+                    self.send_data(blank)
+                except Exception as e:
+                    continue
+
+                
+            
             serial_status = self.serial_status.value
             iracing_status = self.iracing_status.value
             port = self.port
@@ -196,7 +209,8 @@ class Robot:
     def data_pull_loop(self):        
         while True:
             if (self.iracing_status == IRacingStatus.CONNECTED_ON_TRACK) and  (self.serial_status == SerialStatus.CONNECTED):
-                text = get_text(self.ir)
+                text = self.get_text()
+                self.last_message = self.ir['SessionTime']
                 self.data_queue.put(text)
                 time.sleep(.1)
                 self.last_message = text
@@ -213,14 +227,17 @@ class Robot:
                 if not self.data_queue.empty():
                     texts.append(self.data_queue.get())
                     
-                if len(texts) > 5:
+                if len(texts) > 0:
                     data = ''.join(texts)+ '\n'
-                    self.serial.write(data.encode()) 
+                    self.send_data(data)
                     texts = []
             else: 
-                
                 return 
-                    
+
+    def send_data(self, data):
+        self.serial.write(data.encode()) 
+        return
+    
 
         
     def set_port(self):
@@ -245,7 +262,32 @@ class Robot:
 
 handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, os.getpid())
 win32process.SetPriorityClass(handle, win32process.REALTIME_PRIORITY_CLASS) # you could set this to REALTIME_PRIORITY_CLASS etc.
+instructions = """
+Instructions:
 
+
+1. Make Sure this is your computer that will run iRacing. 
+2. Plug in the RyGuy Adapter. The status will change to "RyGuy:Connected" from "RyGuy:Device not Found""
+3. On your Phone Connect to the Adapter via Bluetooth. 
+
+Next is:
+SOLOSTORM SETTINGS
+
+Under Application Settings:
+
+GPS/Logger >
+     GPS/ Logger Source 
+     > Select iRacing + Bluetooth Telemetry
+     Bluetooth Device
+     > Select RyGuysAdapter (You might need to connect your phone to it first)
+
+Vehicle Telemetry>
+Select None / GPS Logger
+
+
+
+"""
 if __name__ == '__main__':
+    print("Welcome To RyGuy Version 1.0 - Beta")
     r = Robot()
     r.run()
--- iracing.py
+++ iracing.py
@@ -23,3 +23,16 @@ def sim_is_on():
     if 'running:0' in r.text:
         return False
     return False
+
+def get_blank():
+    global blank
+    parts = blank.split(',')
+    if parts[2] == '0':
+        parts[2] = '1'
+    else:
+        parts[2] = '0'
+    blank = ','.join(parts)
+    return blank
+    
+
+blank = """$IRTEL,TIME,0,0,0.032359,1,0.000000,0.000000,0.000000,0.002766,0.000000,0.000000,0.109816,300.000000,-0.001262,0.002462,-0.000002,0.290551,-0.065876,9.795001,-0.001526,0.000147,0.008782,0.004638,0.005359,1.992813"""
